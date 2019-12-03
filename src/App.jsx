b2ac7a9a69e5c02b0180cd3954620feded590181
William Gendron <williamcgendron@gmail.com>
2019-12-02 20:38:07 -0500

    First commit brand new React Express App

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+﻿import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+import List from "./components/List";
+//TODO Web Template Studio: Add routes for your new pages here.
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Switch>
+          <Route exact path = "/">
+            <Redirect to="/List" />
+          </Route>
+          <Route path = "/List" component = { List } />
+        </Switch>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
